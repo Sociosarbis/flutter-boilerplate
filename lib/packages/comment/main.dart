08cd03eef7a6c0bd53d00c53dd8ff71e5098373a
Sociosarbis <136657577@qq.com>
2021-02-22 00:15:24 +0800

    feat: :sparkles: 回复输入框

--- lib/packages/comment/main.dart
+++ lib/packages/comment/main.dart
@@ -7,17 +7,102 @@ class Main extends StatefulWidget {
   MainState createState() => MainState();
 }
 
-class MainState extends State<Main> {
+class MainState extends State<Main> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<double> _animation;
+  @override
+  void initState() {
+    _controller =
+        AnimationController(duration: Duration(milliseconds: 250), vsync: this);
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     List<CommentModel.Comment> model =
         data.map((item) => CommentModel.Comment.fromJson(item)).toList();
     return Scaffold(
         appBar: AppBar(title: Text('Comment')),
-        body: Align(
-            alignment: Alignment.topCenter,
-            child: Column(
-                children: [for (var data in model) Comment(data: data)])));
+        body: GestureDetector(
+            onTap: () {
+              FocusScope.of(context).unfocus();
+              _controller.forward();
+            },
+            child: Stack(children: [
+              Column(children: [for (var data in model) Comment(data: data)]),
+              Positioned(
+                  left: 0,
+                  bottom: 0,
+                  child: AnimatedBuilder(
+                      animation: _animation,
+                      builder: (context, child) => Container(
+                          width: MediaQuery.of(context).size.width,
+                          transform: Transform.translate(
+                                  offset: Offset(0, 192 * _animation.value))
+                              .transform,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Color(0xffd9d9d9),
+                                  blurRadius: 5,
+                                  offset: Offset(0, -1))
+                            ],
+                          ),
+                          child: SingleChildScrollView(
+                              padding: EdgeInsets.all(10),
+                              child: Container(
+                                  constraints: BoxConstraints(minHeight: 172),
+                                  child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text('添加新回复',
+                                            style: TextStyle(
+                                                color: Color(0xff3399ff))),
+                                        Container(
+                                            margin: EdgeInsets.symmetric(
+                                                vertical: 3),
+                                            child: TextField(
+                                                maxLines: null,
+                                                decoration: InputDecoration(
+                                                  enabledBorder:
+                                                      OutlineInputBorder(
+                                                          borderSide: BorderSide(
+                                                              color: Color(
+                                                                  0xffd9d9d9))),
+                                                  focusedBorder:
+                                                      OutlineInputBorder(
+                                                          borderSide: BorderSide(
+                                                              color: Color
+                                                                  .fromRGBO(
+                                                                      240,
+                                                                      145,
+                                                                      153,
+                                                                      0.6))),
+                                                ))),
+                                        SizedBox(
+                                            width: 71,
+                                            child: RaisedButton(
+                                              elevation: 0,
+                                              padding: EdgeInsets.all(0),
+                                              color: Color(0xff319abf),
+                                              textColor: Colors.white,
+                                              child: Text('加上去'),
+                                              onPressed: () {
+                                                FocusScope.of(context)
+                                                    .unfocus();
+                                              },
+                                            ))
+                                      ]))))))
+            ])));
   }
 }
 
