f4c1454e33f2912fc053294017b474684e6abeeb
sociosarbis <136657577@qq.com>
2021-07-17 23:53:43 +0800

    refactor: :recycle: flutter hooks

--- lib/packages/bgm/components/login.dart
+++ lib/packages/bgm/components/login.dart
@@ -5,57 +5,49 @@ import 'package:flutter/gestures.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_boilerplate/stores/user.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import '../../../stores/user.dart';
 import 'package:webview_cookie_manager/webview_cookie_manager.dart';
 
-class Main extends StatefulWidget {
+const String url = 'https://bgm.tv/login';
+const String userAgent =
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 Edg/88.0.705.68';
+
+class Main extends HookWidget {
   final void Function(Map<String, String> cookies) onLogin;
   Main({@required this.onLogin});
-  @override
-  MainState createState() => MainState();
-}
-
-class MainState extends State<Main> {
-  Completer<WebViewController> _controller = Completer<WebViewController>();
-  String _url = 'https://bgm.tv/login';
-  String userAgent =
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 Edg/88.0.705.68';
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<UserStore>(
-        builder: (context, userStore, child) => WebView(
-              initialUrl: _url,
-              userAgent: userAgent,
-              javascriptMode: JavascriptMode.unrestricted,
-              debuggingEnabled: true,
-              onWebViewCreated: (controller) {
-                _controller.complete(controller);
-                Provider.of<UserStore>(context, listen: false).isLogining =
-                    true;
-              },
-              onPageFinished: (_) {
-                Provider.of<UserStore>(context, listen: false).isLogining =
-                    false;
-              },
-              navigationDelegate: (request) async {
-                Provider.of<UserStore>(context, listen: false).isLogining =
-                    true;
-                final cookies = (await WebviewCookieManager().getCookies(_url))
-                    .fold<Map<String, String>>(Map<String, String>(),
-                        (acc, item) {
-                  acc[item.name] = item.value;
-                  return acc;
-                });
-                Provider.of<UserStore>(context, listen: false).isLogining =
-                    false;
-                userStore.setCookie(cookies);
-                widget.onLogin(cookies);
-                return NavigationDecision.prevent;
-              },
-              gestureRecognizers: Set()
-                ..add(Factory<VerticalDragGestureRecognizer>(
-                    () => VerticalDragGestureRecognizer())),
-            ));
+    final controller = useRef<WebViewController>(null);
+    final userStore = useUserStore(false);
+    return WebView(
+      initialUrl: url,
+      userAgent: userAgent,
+      javascriptMode: JavascriptMode.unrestricted,
+      debuggingEnabled: true,
+      onWebViewCreated: (_controller) {
+        controller.value = _controller;
+        userStore.isLogining = true;
+      },
+      onPageFinished: (_) {
+        userStore.isLogining = false;
+      },
+      navigationDelegate: (request) async {
+        userStore.isLogining = true;
+        final cookies = (await WebviewCookieManager().getCookies(url))
+            .fold<Map<String, String>>(Map<String, String>(), (acc, item) {
+          acc[item.name] = item.value;
+          return acc;
+        });
+        userStore.isLogining = false;
+        userStore.setCookie(cookies);
+        onLogin(cookies);
+        return NavigationDecision.prevent;
+      },
+      gestureRecognizers: Set()
+        ..add(Factory<VerticalDragGestureRecognizer>(
+            () => VerticalDragGestureRecognizer())),
+    );
   }
 }
--- lib/stores/user.dart
+++ lib/stores/user.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:provider/provider.dart';
 
 class UserStore with ChangeNotifier {
   Map<String, String> cookie = Map<String, String>();
@@ -29,4 +31,10 @@ class UserStore with ChangeNotifier {
     cookie = c;
     notifyListeners();
   }
+}
+
+UserStore useUserStore(bool listen) {
+  return useMemoized(() {
+    return Provider.of<UserStore>(useContext(), listen: listen);
+  }, [listen]);
 }
